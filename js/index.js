8587b46ae966be2d3eb01afb34c3e98054a7942e
Workezy <109183071+Workezy@users.noreply.github.com>
2023-03-26 22:45:55 +0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,21 +1,38 @@
-var swiper = new Swiper('.swiper', {
-    slidesPerView: 3,
-    loop: true,
-    speed: 900,
-    centeredSlides: true,
-    slideToClickedSlide: true,
-    spaceBetween: 70,
-    direction: getDirection(),
-    on: {
-      resize: function () {
-        swiper.changeDirection(getDirection());
-      },
+let swiper = new Swiper(".swiper", {
+  slidesPerView: 3,
+  loop: true,
+  speed: 900,
+  centeredSlides: true,
+  slideToClickedSlide: true,
+  spaceBetween: 70,
+  direction: getDirection(),
+  on: {
+    resize: function () {
+      swiper.changeDirection(getDirection());
     },
-  });
+  },
+});
 
-  function getDirection() {
-    var windowWidth = window.innerWidth;
-    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+function getDirection() {
+  var windowWidth = window.innerWidth;
+  var direction = window.innerWidth <= 760 ? "vertical" : "horizontal";
 
-    return direction;
+  return direction;
+}
+
+let popupBg = document.querySelector(".popup_request_bg");
+let popup = document.querySelector(".popup_request");
+let orderBtn = document.querySelector(".buy_btn");
+
+orderBtn.addEventListener("click", () => {
+  popupBg.classList.add("active");
+  popup.classList.add("active");
+});
+popupBg.addEventListener("click", (e) => {
+  if (e.target.dataset.toggle === "popup_bg") {
+    popupBg.classList.remove("active");
+    popup.classList.remove("active");
+  } else {
+    return;
   }
+});
